75af2f8c8e524c3af3f389c874691029cd65f1ab
Keks <keks@htmlacademy.ru>
2023-04-26 21:09:35 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/script.js
+++ 5/js/script.js
@@ -1,8 +1,10 @@
 let headerMain = document.querySelector('.main-header');
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
+let mapIframe = document.querySelector('iframe.map__iframe');
 
 headerMain.classList.remove('main-header--nojs');
+mapIframe?.classList.remove('map__iframe--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
